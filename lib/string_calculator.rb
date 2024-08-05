84e10e9af5a896b79b47fc5f3e84c350d342d20c
rishistrive <rishidevtechh09@gmail.com>
2024-08-05 16:49:37 +0530

    refactor add method for handle numbers with multiple delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,23 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
 
-    string.split(/,|\n/).map(&:to_i).sum
+    numbers = get_numbers(string)
+    numbers.sum
+  end
+
+  private
+
+  def get_numbers(string)
+    separators = [',', "\n"]
+    if check_delimiter?(string)
+      delimiter = string.split("\n")[0][2..-1]
+      separators << delimiter
+    end
+
+    string.split(/#{Regexp.union(separators)}/).map(&:to_i)
+  end
+
+  def check_delimiter?(string)
+    string.start_with?('//')
   end
 end
