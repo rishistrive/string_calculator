1a1d90772a081ede8398a3768725b5d78c24cce9
rishistrive <rishidevtechh09@gmail.com>
2024-08-05 14:34:53 +0530

    Add test cases for single numeric digit

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe StringCalculator do
         expect(calculator.add('')).to eq(0)
       end
     end
+
+    context 'when input is a single number' do
+      it 'returns the number' do
+        calculator = StringCalculator.new
+        expect(calculator.add('5')).to eq(5)
+      end
+    end
   end
 end
